a89bdb15651aacb54609d246c895bc12bbd525a5
cbymar <christophermartin@Christophers-MacBook-Pro.local>
2021-01-13 13:16:18 -0800

    change url patterns

--- polls/urls.py
+++ polls/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-path(r'^$', views.IndexView.as_view(), name='index' ), 
-path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-path(r'^(?P<pk>[0-9]+)/results$', views.ResultsView.as_view(), name='results'),
-path(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name='vote'),
+path('', views.IndexView.as_view(), name='index' ), 
+path('/', views.DetailView.as_view(), name='detail'),
+path('/results', views.ResultsView.as_view(), name='results'),
+path('/vote', views.vote, name='vote'),
 ]
