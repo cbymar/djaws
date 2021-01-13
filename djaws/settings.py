309dce5c75d669f5257e1018c0a452766bc5b109
cbymar <christophermartin@Christophers-MacBook-Pro.local>
2021-01-13 13:55:00 -0800

    add allowed host

--- djaws/settings.py
+++ djaws/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = creds['DJAWS']['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://ec2-34-209-131-122.us-west-2.compute.amazonaws.com']
 
 
 # Application definition
