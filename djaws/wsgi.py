d47c3dfb1243e622e0ed765d79f6d54d024d4382
cbymar <christophermartin@Christophers-MacBook-Pro.local>
2021-01-14 09:55:14 -0800

    change wsgi

--- djaws/wsgi.py
+++ djaws/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/var/www/djaws')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djaws.settings")
 
 application = get_wsgi_application()
